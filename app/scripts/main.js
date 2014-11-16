245c3940f9163e30bcb41823a46f51cba1fdabb0
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-11-16 17:25:35 -0500

    Init

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+(function () {
+  console.log('The Start');
+
+  var initMap = function initMapF(){
+    var mapOptions = {
+      center: { lat: -34.397, lng: 150.644},
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initMap);
+
+})();
