fee8094f8b107291faac6d8924b27f226ee668a6
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-11-16 20:05:37 -0500

    worldcities parsin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,81 @@
   console.log('The Start');
 
   var initMap = function initMapF(){
+
+    var styles = [
+      // {
+      //   stylers: [
+      //     { hue: "#00ffe6" },
+      //     { saturation: -20 }
+      //   ]
+      // },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { lightness: 100 },
+          { visibility: "simplified" }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "all",
+        stylers: [
+          { visibility: "off" }
+        ]     
+      },
+      {
+        featureType: 'administrative.neighborhood'
+      }
+    ];
+
+    var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Culture Map"});
+    
     var mapOptions = {
       center: { lat: -34.397, lng: 150.644},
-      zoom: 8
+      zoom: 3,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      }
     };
+
     var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+         map.setCenter(initialLocation);
+      });
+    }
   }
 
   google.maps.event.addDomListener(window, 'load', initMap);
 
 })();
+
+//want a list of the world's cities
+  //must contain population
+  //and log and lat
+
+//create 2 inputs
+  //one with population limit
+  //one with distance between cities
+
+  //if population is over x
+  //and is distance is greater than y to next city
+
+  //mark city
+
+  
